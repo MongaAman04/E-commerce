8def4b6763ef75edf13167f0f73fda62c6d2d83d
Aman Monga <amanmonga000@gmail.com>
2024-12-09 00:52:19 +0530

    add authentication

---
+++ script/auth.js
@@ -0,0 +1,75 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC5i8p6D7RaD_Nh58asvIRF0BN_R21HDDI",
+    authDomain: "e-commerce-1b971.firebaseapp.com",
+    projectId: "e-commerce-1b971",
+    storageBucket: "e-commerce-1b971.firebasestorage.app",
+    messagingSenderId: "614888943452",
+    appId: "1:614888943452:web:3c4b7f1094e0370a7edb99",
+    measurementId: "G-MQ74QN36ZN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const button = document.querySelector("#submit")
+
+button.addEventListener("click", function () {
+    const email = document.querySelector("#mail").value;
+    const repassword = document.querySelector("#repass").value;
+    const password = document.querySelector("#pass").value;
+    const warninng = document.querySelectorAll(".container span")
+    if (password == repassword) {
+        const auth = getAuth();
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                window.location.href = "index.html"
+
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.error(errorMessage);
+
+            });
+    } else {
+        warninng.forEach(e => {
+            e.style.display = "block";
+        })
+    }
+
+})
+const change = document.querySelector(".change");
+change.addEventListener("click", () => {
+    const firstHalf = document.querySelector(".first-half");
+    const secondHalf = document.querySelector(".second-half");
+    firstHalf.style.transform = " translateX(100%)";
+    firstHalf.style.transition = "1s";
+    secondHalf.style.transform = "translateX(-100%)";
+    secondHalf.style.transition = "1s";
+    secondHalf.querySelector("h1").innerText = "Welcome back!"
+    secondHalf.querySelector("p").innerText = "login to continue shopping..."
+    firstHalf.innerHTML = `<div class="container">
+            <h1>LogIn</h1>
+            <label for="email">Email Adddress</label>
+            <input type="email" name="email" id="mail">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="pass">
+            <span><p>*Password does not match</p></span>
+           <button id="submit">LogIn</button>
+           <p class="change">New User? SignUp</p>
+                                                                                      
+        </div>`
+    })
+    
--- script/script.js
+++ script/script.js
@@ -1,35 +1,36 @@
-// "use strict"
+"use strict"
 let bestSeller = document.querySelector(".best-seller")
 let loadMore = document.querySelector(".load button")
-let body = document.querySelector("body")
+let mainBody = document.querySelector(".mainBody")
+const originaltext = mainBody.innerHTML;
 const collection = [
   {
     item: "Punjbai Juttii",
     mainimg: "http://127.0.0.1:5500/images/IMG-20230723-WA0003.jpg",
     description: "",
     price: "100",
-
+    
   },
   {
     item: "Punjbai Jutti 2",
     mainimg: "http://127.0.0.1:5500/images/IMG-20230723-WA0004.jpg",
     description: "",
     price: "200",
-
+    
   },
   {
     item: "Punjbai Jutti 3",
     mainimg: "http://127.0.0.1:5500/images/IMG-20230723-WA0006.jpg",
     description: "",
     price: "300",
-
+    
   },
   {
     item: "Punjbai Jutti 4",
     mainimg: "http://127.0.0.1:5500/images/IMG-20230723-WA0005.jpg",
     description: "",
     price: "400",
-
+    
   },
 ]
 async function topSales() {
@@ -41,81 +42,53 @@ async function topSales() {
       let price = e.querySelector(".price")
       let description = e.querySelector(".description")
       let name = e.querySelector(".name-of-p p")
-      body.innerHTML += `<section class=" top-txt ">
-        <div class="head container ">
-            <div class="head-txt ">
-                <p>Free shipping, 30-day return or refund guarantee.</p>
-            </div>
-            <div class="sing_in_up ">
-                <a href="# ">SIGN IN</a>
-                <a href="# ">SIGN UP</a>
-            </div>
-        </div>
-    </section>
-    <nav class="navbar">
-        <div class="navbar-container">
-            <input type="checkbox" name="" id="checkbox">
-            <div class="hamburger-lines">
-                <span class="line line1"></span>
-                <span class="line line2"></span>
-                <span class="line line3"></span>
-            </div>
-            <ul class="menu-items">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#sellers">Shop</a></li>
-                <li><a href="#news">Blog</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
-            <div class="logo">
-                <img src="https://i.postimg.cc/TP6JjSTt/logo.webp" alt="">
-            </div>
-        </div>
-    </nav>
-    <section class="product-details">
-        <div class="product-images">
-            <div class="other-images">
-                <div class="s-img">
-                    <img src="images/IMG-20230723-WA0003.jpg" alt="">
-                </div>
-                <div class="s-img">
-                    <img src="images/IMG-20230723-WA0004.jpg" alt="">
-                </div>
-                <div class="s-img">
-                    <img src="images/IMG-20230723-WA0005.jpg" alt="">
-                </div>
-                <div class="s-img">
-                    <img src="images/IMG-20230723-WA0006.jpg" alt="">
-                </div>
-                <div class="s-img">
-                    <img src="images/IMG-20230726-WA0007.jpg" alt="">
-                </div>
-                </div>
-                <div class="big-image">
-                    <img src='${mainImage}' alt="">
-                </div>
-            </div>
-            <div class="details"> 
-                <h1></h1>
-                <p style="color: var(--grey);"></p>
-                <h2>Special Price</h2>
-                <h1 style="font-size: 15px; color: red;"><span style="font-size: 25px; color: rgb(73, 53, 250);">${price}</span> <del style="font-size: 18px; color: var(--grey); font-style: italic;">3000</del> 75%</h1>
-                <p>Lorem ipsum dolor sit amet consectetur.</p>
-                <h2>Select your Size According to size Chart</h2>
-                <div class="sizes">
-                    <div class="size" id="three">3</div>
-                    <div class="size" id="four">4</div>
-                    <div class="size" id="five">5</div>
-                    <div class="size" id="six">6</div>
-                    <div class="size" id="seven">7</div>
-            </div>
-            <span class="buttons">
-                <button class="buynow">Buy Now</button>
-                <!-- <button class="addToCart">Add to Cart</button> -->
-            </span>
 
-        </div>
-    </section>`
+    //   mainBody.innerHTML = `<section class="product-details">
+    //     <div class="product-images">
+    //         <div class="other-images">
+    //             <div class="s-img">
+    //                 <img src="images/IMG-20230723-WA0003.jpg" alt="">
+    //             </div>
+    //             <div class="s-img">
+    //                 <img src="images/IMG-20230723-WA0004.jpg" alt="">
+    //             </div>
+    //             <div class="s-img">
+    //                 <img src="images/IMG-20230723-WA0005.jpg" alt="">
+    //             </div>
+    //             <div class="s-img">
+    //                 <img src="images/IMG-20230723-WA0006.jpg" alt="">
+    //             </div>
+    //             <div class="s-img">
+    //                 <img src="images/IMG-20230726-WA0007.jpg" alt="">
+    //             </div>
+    //             </div>
+    //             <div class="big-image">
+    //                 <img src="${mainImage}" alt="">
+    //             </div>
+    //         </div>
+    //         <div class="details">
+    //             <h1>${name.innerHTML}</h1>
+    //             <p style="color: var(--grey);">${description}</p>
+    //             <h2>Special Price</h2>
+    //             <h1 style="font-size: 15px; color: red;"><span style="font-size: 25px; color: rgb(73, 53, 250);">${price}</span> <del style="font-size: 18px; color: var(--grey); font-style: italic;">3000</del> 75%</h1>
+    //             <p>Lorem ipsum dolor sit amet consectetur.</p>
+    //             <h2>Select your Size According to size Chart</h2>
+    //             <div class="sizes">
+    //                 <div class="size" id="three">3</div>
+    //                 <div class="size" id="four">4</div>
+    //                 <div class="size" id="five">5</div>
+    //                 <div class="size" id="six">6</div>
+    //                 <div class="size" id="seven">7</div>
+    //         </div>
+    //         <span class="buttons">
+    //             <button class="buynow">Buy Now</button>
+    //             <!-- <button class="addToCart">Add to Cart</button> -->
+    //         </span>
 
+    //     </div>
+    // </section>`
+    
+    
     })
 
   })
